a929a333f74829d2c527d9eac38f57ece02fac37
Felipe Terrana Cazetta <felipetcazetta@gmail.com>
2021-01-12 18:33:09 -0300

    Implement joystick input logic

--- cpu/rp2A03/joystick.c
+++ cpu/rp2A03/joystick.c
@@ -1,12 +1,39 @@
 #include "joystick.h"
 
 #include <stdlib.h>
+#include "../../flag_ops.h"
+
+enum JoystickButton {
+    JOY_A = 0,
+    JOY_B = 1,
+    JOY_SELECT = 2,
+    JOY_START = 3,
+    JOY_UP = 4,
+    JOY_DOWN = 5,
+    JOY_LEFT = 6,
+    JOY_RIGHT = 7,
+
+    JOY_END_OF_LOOP = 8
+};
+
+enum Player {
+    PLAYER_1 = 0,
+    PLAYER_2 = 1
+};
 
 
 
 struct joystick__ {
-
+    uint8_t strobe;
+    enum JoystickButton nextToRead;
 };
+
+
+
+static uint8_t buttonStatus(Joystick* joystick, enum JoystickButton button, enum Player player)
+{
+    return 0x0;
+}
 
 
 
@@ -27,7 +54,17 @@ void joystickShutdown(Joystick* joystick)
 
 uint16_t joystickRead(Joystick* joystick, uint16_t address, uint8_t* value)
 {
-    // TODO joystickRead
+    *value = 0x40;
+
+    if(joystick->strobe == 0 && joystick->nextToRead < JOY_END_OF_LOOP)
+        *value |= buttonStatus(joystick, joystick->nextToRead++, address % 2);
+
+    else if(joystick->strobe == 0 && joystick->nextToRead >= JOY_END_OF_LOOP)
+        *value |= 0x01;
+
+    else if(joystick->strobe == 1)
+        *value |= buttonStatus(joystick, JOY_A, address % 2);
+
     return 0;
 }
 
@@ -35,6 +72,9 @@ uint16_t joystickRead(Joystick* joystick, uint16_t address, uint8_t* value)
 
 uint16_t joystickWrite(Joystick* joystick, uint16_t address, uint8_t value)
 {
-    // TODO joystickWrite
+    if(getFlagValue(joystick->strobe, 0x01) == 1 && getFlagValue(value, 0x01) == 0)
+        joystick->nextToRead = JOY_A;
+
+    joystick->strobe = value;
     return 0;
 }
